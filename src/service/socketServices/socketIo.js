c06698d6391a40cf2ee948f62501f8996b31b3c0
Prince <PrincePR8920@gmail.com>
2023-12-13 13:47:56 +0530

    fixed some bugs in sokcet

--- src/service/socketServices/socketIo.js
+++ src/service/socketServices/socketIo.js
@@ -12,7 +12,7 @@ const { Server } = require("socket.io"),
 function mySocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:4000",
+      origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
   });
